89f6489ab44797fc6bee06ab439466516e01dda3
Jon <jonathon.lockwood@networkedweb.com>
2026-09-10 14:53:41 +0930

    feat(centurion_erp): Add Claim keys for keycloak SSO
    ref: #22 #1

--- manifests/centurion_erp/components/sso-keycloak/70.social_auth_keycloak.py
+++ manifests/centurion_erp/components/sso-keycloak/70.social_auth_keycloak.py
@@ -6,6 +6,8 @@ SOCIAL_AUTH_KEYCLOAK_SECRET = ''        # oidc secret key
 SOCIAL_AUTH_KEYCLOAK_PUBLIC_KEY = ''    # public key
 SOCIAL_AUTH_KEYCLOAK_AUTHORIZATION_URL = 'https://<domain.tkd>/realms/<realm name>/protocol/openid-connect/auth'
 SOCIAL_AUTH_KEYCLOAK_ACCESS_TOKEN_URL = 'https://<domain.tkd>/realms/<realm name>/protocol/openid-connect/token'
+SOCIAL_AUTH_KEYCLOAK_REQUIRED_CLAIM_NAME = 'roles'
+SOCIAL_AUTH_KEYCLOAK_REQUIRED_CLAIM_VALUE = 'Centurion-User'
 
 SSO_ENABLED = True
 
